2cbb431fd3e99b42dfd43e3f43929d746fd6c6bf
agchanes <ag.chanes@gmail.com>
2021-08-19 23:51:14 -0300

    página productos

--- js/products.js
+++ js/products.js
@@ -3,4 +3,43 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
 
+
+    fetch(PRODUCTS_URL)
+        .then(result => result.json())
+        .then(data => {
+
+            let i = 0
+            while (i < data.length) {
+
+                let productNumber = data[i]
+
+                let name = productNumber.name
+                let description = productNumber.description
+                let cost = productNumber.cost
+                let currency = productNumber.currency
+                let img = productNumber.imgSrc
+                let soldcount = productNumber.soldCount
+
+
+
+
+
+                document.getElementById("list-container").innerHTML +=
+                `
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + img + `" alt="` + description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ name + `</h4>
+                            <small class="text-muted"> Vendidos: ` + soldcount +`<hr>`+ currency +` `+ cost +`</small>
+                        </div>
+                        <p class="mb-1">` + description + `</p>
+                    </div>
+                </div>
+                `
+                i++
+            }
+        })
 });
