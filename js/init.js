5e2653e3dd3fc65e81bce93173d213e86dd5dd06
agchanes <ag.chanes@gmail.com>
2021-11-28 23:20:26 -0300

    links sin modificar y nombre de archivo app.js modificado

--- js/init.js
+++ js/init.js
@@ -1,93 +1,12 @@
-const localhost = "http://localhost:3000/"
-// const express = require('express');
-// const app = express();
+const CATEGORIES_URL = "https://japdevdep.github.io/ecommerce-api/category/all.json";
+const PUBLISH_PRODUCT_URL = "https://japdevdep.github.io/ecommerce-api/product/publish.json";
+const CATEGORY_INFO_URL = "https://japdevdep.github.io/ecommerce-api/category/1234.json";
+const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json";
+const PRODUCT_INFO_URL = "https://japdevdep.github.io/ecommerce-api/product/5678.json";
+const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
+const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
+const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
-console.log(localhost);
-
-// const categories = require('./categories.json');
-// const products = require('./products.json');
-// const cart_buy = require('./cart_buy.json');
-// const cart_info = require('./cart_info.json');
-// const product_info_comments = require('./product_info_comments.json');
-// const product_info = require('./product_info.json');
-// const categories_info = require('./categories_info.json');
-// const publish = require('./publish.json');
-
-const CATEGORIES_URL = localhost+"categories";
-const PUBLISH_PRODUCT_URL = localhost+"publish";
-const CATEGORY_INFO_URL = localhost+"categories_info";
-const PRODUCTS_URL = localhost+"products";
-const PRODUCT_INFO_URL = localhost+"product_info";
-const PRODUCT_INFO_COMMENTS_URL = localhost+"product_info_comments";
-const CART_INFO_URL = localhost+"cart_info";
-const CART_BUY_URL = localhost+"cart_buy";
-
-console.log(PRODUCTS_URL);
-
-
-/* 
-function levantarServidor(json){
-
-  app.get('/'+json'', function(req, res){
-    res.setHeader("Access-Control-Allow-Origin","*")
-    res.set('Content-Type', 'application/json')
-    res.send(json);
-  })
-}
-*/
-
-
-// app.get('/products', function(req, res){
-//     res.setHeader("Access-Control-Allow-Origin","*")
-//     res.set('Content-Type', 'application/json')
-//     res.send(products);
-// })
-
-// app.get('/categories', function(req, res){
-//     res.setHeader("Access-Control-Allow-Origin","*")
-//     res.set('Content-Type', 'application/json')
-//     res.send(categories);
-// })
-
-// app.get('/cart_buy', function(req, res){
-//     res.setHeader("Access-Control-Allow-Origin","*")
-//     res.set('Content-Type', 'application/json')
-//     res.send(cart_buy);
-// })
-
-// app.get('/cart_info', function(req, res){
-//     res.setHeader("Access-Control-Allow-Origin","*")
-//     res.set('Content-Type', 'application/json')
-//     res.send(cart_info);
-// })
-
-// app.get('/product_info_comments', function(req, res){
-//     res.setHeader("Access-Control-Allow-Origin","*")
-//     res.set('Content-Type', 'application/json')
-//     res.send(product_info_comments);
-// })
-
-// app.get('/product_info', function(req, res){
-//     res.setHeader("Access-Control-Allow-Origin","*")
-//     res.set('Content-Type', 'application/json')
-//     res.send(product_info);
-// })
-
-// app.get('/categories_info', function(req, res){
-//     res.setHeader("Access-Control-Allow-Origin","*")
-//     res.set('Content-Type', 'application/json')
-//     res.send(categories_info);
-// })
-
-// app.get('/publish', function(req, res){
-//     res.setHeader("Access-Control-Allow-Origin","*")
-//     res.set('Content-Type', 'application/json')
-//     res.send(publish);
-// })
-
-// app.listen(3000, function(){
-//     console.log('servidor levantado!');
-// })
 
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
